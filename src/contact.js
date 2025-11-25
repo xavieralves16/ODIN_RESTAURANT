0570fdc02390cf060d6e7a6d04207642da37ce18
Xavier <xavieralves162@gmail.com>
2025-11-25 12:14:19 +0000

    Updated contact

--- src/contact.js
+++ src/contact.js
@@ -2,15 +2,21 @@ export default function loadContact() {
   const content = document.getElementById("content");
 
   const contactDiv = document.createElement("div");
+  contactDiv.className = "contact";
 
   const title = document.createElement("h1");
   title.textContent = "Contact Us";
 
-  const phone = document.createElement("p");
-  phone.textContent = "📞 123-456-789";
+  const info = document.createElement("div");
+  info.className = "info";
+  info.innerHTML = `
+    📞 <strong>123-456-789</strong><br><br>
+    📧 <a href="mailto:info@restaurant.com">info@restaurant.com</a><br><br>
+    📍 Lisbon, Portugal
+  `;
 
   contactDiv.appendChild(title);
-  contactDiv.appendChild(phone);
+  contactDiv.appendChild(info);
 
   content.appendChild(contactDiv);
 }
