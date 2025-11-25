69f71a3b198ca0cf1a8914b39a7b5f6bfdb7341f
Xavier <xavieralves162@gmail.com>
2025-11-25 11:13:13 +0000

    Added tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import loadHome from "./home.js";
+import loadMenu from "./menu.js";
+import loadContact from "./contact.js";
+
+function clearContent() {
+  const content = document.getElementById("content");
+  content.textContent = "";
+}
+
+function addNavEvents() {
+  document.getElementById("homeBtn").addEventListener("click", () => {
+    clearContent();
+    loadHome();
+  });
+
+  document.getElementById("menuBtn").addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+  });
+
+  document.getElementById("contactBtn").addEventListener("click", () => {
+    clearContent();
+    loadContact();
+  });
+}
 
 function init() {
+  addNavEvents();
   loadHome();
 }
 
